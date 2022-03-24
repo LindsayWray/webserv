a563f585fea626da9240f52466e16c02ec7e3255
Tijmen t El-baradi <tel-bara@f0r6s8.codam.nl>
2022-03-24 12:47:20 +0100

    responder works with in function constructed response

--- srcs/server/testServer.cpp
+++ srcs/server/testServer.cpp
@@ -55,27 +55,27 @@ void webserv::testServer::_responder(int fd, HTTPResponseMessage response) {
 		// send( fd, "\n", 1, 0 );
 
 	/* FIRST ATTEMPT AT RESPONSE IMPL */
-		// std::cout << "sending response" << std::endl;
+		std::cout << "sending response" << std::endl;
 
-		// const std::string& responseStr = response.toString();
-		// send( fd, responseStr.c_str(), responseStr.length(), 0 );
+		const std::string& responseStr = response.toString();
+		send( fd, responseStr.c_str(), responseStr.length(), 0 );
 
 	/* TEST WITH CONSTRUCTED RESPONSE */
-		std::ifstream outfile;
-		std::string line;
-		std::string body;
-		outfile.open("var/www/html/test.html");
-		while( std::getline( outfile, line ) ) {
-			body += line;
-		}
-		HTTPResponseMessage sut;
-		sut.addStatus(HTTPResponseMessage::NOT_FOUND)
-			.addTypeCat(HTTPResponseMessage::TEXT)
-			.addTypeExt("plain")
-			.addLength(body.length())
-			.addBody(body);
-		std::cout << sut.toString();
-		send( fd, sut.toString().c_str(), sut.toString().length(), 0);
+		// std::ifstream outfile;
+		// std::string line;
+		// std::string body;
+		// outfile.open("var/www/html/test.html");
+		// while( std::getline( outfile, line ) ) {
+		// 	body += line;
+		// }
+		// HTTPResponseMessage sut;
+		// sut.addStatus(HTTPResponseMessage::NOT_FOUND)
+		// 	.addTypeCat(HTTPResponseMessage::TEXT)
+		// 	.addTypeExt("html")
+		// 	.addLength(body.length())
+		// 	.addBody(body);
+		// std::cout << sut.toString();
+		// send( fd, sut.toString().c_str(), sut.toString().length(), 0);
 }
 
 void webserv::testServer::launch() {
